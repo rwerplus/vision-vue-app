c7479cc3d8821e1acd8abd93e510297cc3228cfa
kenny yori <localfeng@163.com>
2021-11-11 23:03:25 +0800

    修改egg部分配置

--- vision-egg-serve/app/middleware/robot.ts
+++ vision-egg-serve/app/middleware/robot.ts
@@ -1,7 +1,7 @@
-module.exports = (options) => {
+module.exports = options => {
   return async function robotMiddleware(ctx, next) {
     const source = ctx.get('user-agent') || '';
-    const match = options.ua.some((ua) => ua.test(source));
+    const match = (options.ua || []).some(ua => ua.test(source));
     if (match) {
       ctx.status = 403;
       ctx.message = 'Go away, robot!';
