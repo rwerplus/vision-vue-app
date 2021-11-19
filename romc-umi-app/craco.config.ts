1785b589826bc33ad48352e5f40aa9f58e873712
wangguofeng <localfeng@163.com>
2021-11-19 17:22:13 +0800

    使用craco增加热启动

--- romc-umi-app/craco.config.ts
+++ romc-umi-app/craco.config.ts
@@ -1,6 +1,8 @@
 // @ts-ignore
 const CracoLessPlugin = require('craco-less');
+const { whenDev } = require('@craco/craco')
 const CracoAlias = require("craco-alias");
+const fastRefreshCracoPlugin = require('craco-fast-refresh')
 /* craco.config.js */
 module.exports = {
   webpack: {
@@ -10,8 +12,6 @@ module.exports = {
     plugin: CracoAlias,
     options: {
       source: "tsconfig",
-      // baseUrl SHOULD be specified
-      // plugin does not take it from tsconfig
       baseUrl: "./src",
       // tsConfigPath should point to the file where "baseUrl" and "paths" are specified
       tsConfigPath: "./tsconfig.extend.json"
@@ -30,5 +30,12 @@ module.exports = {
         },
       },
     },
+      ...whenDev(
+          () => [
+            {
+              plugin: fastRefreshCracoPlugin,
+            }
+          ]
+      )
   ],
 }
