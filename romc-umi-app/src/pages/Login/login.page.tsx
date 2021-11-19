01f23c1a05662495f09c535627c2c926c54788dd
kenny yori <localfeng@163.com>
2021-11-20 00:57:37 +0800

    添加打包分析插件

--- romc-umi-app/src/pages/Login/login.page.tsx
+++ romc-umi-app/src/pages/Login/login.page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Col, Layout, Row } from 'antd';
-import './login.module.scss';
+import './login.scss';
 import LoginFormComp from "@pages/Login/loginForm.comp";
 
 const LoginPage: React.FC = () => {
@@ -10,8 +10,8 @@ const LoginPage: React.FC = () => {
     <Layout>
       <Content className="contentWrap">
         <Row justify="center" align="middle" className="formContainer">
-          <Col span={6} className={"formMaintain"}>
-            <LoginFormComp />
+          <Col span={18} flex={1} className="formMaintain">
+            <LoginFormComp/>
           </Col>
         </Row>
       </Content>
--- romc-umi-app/src/pages/Login/loginForm.comp.tsx
+++ romc-umi-app/src/pages/Login/loginForm.comp.tsx
@@ -37,7 +37,7 @@ const LoginFormComp: React.FC = () => {
       </Form.Item>
 
       <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
+        <Button type="primary" block={true} htmlType="submit" className="login-form-button">
           Log in
         </Button>
       </Form.Item>
